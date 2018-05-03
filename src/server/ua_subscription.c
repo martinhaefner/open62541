ed45a73532ead35f8c756476d7d37b941952b8d1
Martin Haefner <m.haefner@cab.de>
2018-05-03 15:59:28 +0200

    Fix positioning on UA_EventFieldList

--- src/server/ua_subscription.c
+++ src/server/ua_subscription.c
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  *    Copyright 2015-2017 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
  *    Copyright 2015 (c) Chris Iatrou
@@ -182,6 +182,8 @@ moveNotificationsFromEvents(UA_Server *server, UA_Subscription *sub, UA_EventFie
         UA_EventFilterResult_deleteMembers(&notification->data.event.result); */
         UA_EventFieldList_deleteMembers(&notification->data.event.fields);
         UA_free(notification);
+
+        ++pos;
     }
 }
 #endif
